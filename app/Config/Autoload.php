7a42462449df14ac3842be2380020d8a8256f270
CIMembership Team <support@cimembership.io>
2026-04-10 11:05:16 +0530

    Installer related issues were fixed

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -8,12 +8,12 @@ use CodeIgniter\Config\AutoloadConfig;
 
 class Autoload extends AutoloadConfig
 {
-    public array $psr4 = [
-        APP_NAMESPACE => APPPATH,
-        'Config'      => APPPATH . 'Config',
+    public $psr4 = [
+        'App'    => APPPATH,
+        'Config' => APPPATH . 'Config',
     ];
 
-    public array $classmap = [];
-    public array $files = [];
-    public array $helpers = ['url', 'form', 'html', 'security'];
+    public $classmap = [];
+    public $files = [];
+    public $helpers = ['url', 'form', 'html', 'security'];
 }
--- app/Config/Boot/production.php
+++ app/Config/Boot/production.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 ini_set('display_errors', '0');
-error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
 
 define('CI_DEBUG', false);
 define('CI_ENVIRONMENT', 'production');
